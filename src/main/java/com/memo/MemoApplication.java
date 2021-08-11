daf60ecbe8f9325bfca567c427434172fb27c535
DESKTOP-6P8KEUV\user <dbehdgjs97@naver.com>
2021-08-11 17:51:06 +0900

    210811

--- src/main/java/com/memo/MemoApplication.java
+++ src/main/java/com/memo/MemoApplication.java
@@ -1,13 +1,10 @@
 package com.memo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = "com.*")
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class MemoApplication {
 
