3dd8a0e4697e190260d2c55b061e45a6158190cf
DESKTOP-6P8KEUV\user <dbehdgjs97@naver.com>
2021-08-11 17:09:13 +0900

    초기 환경세팅

---
+++ src/main/java/com/memo/test/Test.java
@@ -0,0 +1,17 @@
+package com.memo.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class Test {
+   
+	@RequestMapping("/test")
+	@ResponseBody
+	public String test() {
+		
+		return "hello world";
+	}
+}
